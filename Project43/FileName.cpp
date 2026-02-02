8eab95472fbf5ad35902c526f5c87ff020b652c8
ggyy <alqaz@outlook.com>
2026-02-02 19:39:05 +0800

    update

--- Project43/FileName.cpp
+++ Project43/FileName.cpp
@@ -10,15 +10,16 @@ using namespace cv;
 int main(void)
 {
     Mat image = imread("1.jpg");
-
+    Mat gray;
     if (image.empty()) 
     {
         SPDLOG_INFO("import file failed");
         return -1;
     }
+	cvtColor(image, gray, COLOR_BGR2GRAY);
     namedWindow("hehe", WINDOW_NORMAL);
     resizeWindow("hehe", 800, 600);
-    imshow("hehe", image);
+    imshow("hehe", gray);
     waitKey(0);
 	SPDLOG_INFO("hello,world {}", fmt::format("{} next", 0.1));
 	return 0;
