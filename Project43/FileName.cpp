655a04922c2fb4636f67dd6e412932b1f0b51e61
ggyy <alqaz@outlook.com>
2026-02-02 19:41:59 +0800

    update

--- Project43/FileName.cpp
+++ Project43/FileName.cpp
@@ -11,15 +11,17 @@ int main(void)
 {
     Mat image = imread("1.jpg");
     Mat gray;
+    Mat edges;
     if (image.empty()) 
     {
         SPDLOG_INFO("import file failed");
         return -1;
     }
 	cvtColor(image, gray, COLOR_BGR2GRAY);
+	Canny(gray, edges, 100, 200);
     namedWindow("hehe", WINDOW_NORMAL);
     resizeWindow("hehe", 800, 600);
-    imshow("hehe", gray);
+    imshow("hehe", edges);
     waitKey(0);
 	SPDLOG_INFO("hello,world {}", fmt::format("{} next", 0.1));
 	return 0;
