745642400a4e1b8f263f9b461725c88611fef385
ggyy <alqaz@outlook.com>
2026-02-02 19:45:04 +0800

    Update FileName.cpp

--- Project43/FileName.cpp
+++ Project43/FileName.cpp
@@ -4,7 +4,9 @@
 #include "tao/pegtl.hpp"
 #include "absl/hash/hash.h"
 #include "opencv4/opencv2/opencv.hpp"
+#include <vector>
 
+using std::vector;
 using namespace cv;
 
 int main(void)
@@ -19,9 +21,15 @@ int main(void)
     }
 	cvtColor(image, gray, COLOR_BGR2GRAY);
 	Canny(gray, edges, 100, 200);
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(edges, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    Mat result = image.clone();
+    drawContours(result, contours, -1, Scalar(0, 255, 0), 2);
     namedWindow("hehe", WINDOW_NORMAL);
     resizeWindow("hehe", 800, 600);
-    imshow("hehe", edges);
+    imshow("hehe", result);
     waitKey(0);
 	SPDLOG_INFO("hello,world {}", fmt::format("{} next", 0.1));
 	return 0;
